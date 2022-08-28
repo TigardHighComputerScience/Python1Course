e1b259c1997686d5f8fab29ddf883078bbfbcc66
Brandon <pacewicbrandon@gmail.com>
2022-08-27 21:03:43 -0700

    Create boolean_if_logical_operators.py

---
+++ references/7-boolean-if-logical-operators/boolean_if_logical_operators.py
@@ -0,0 +1,39 @@
+# True false value
+my_bool = True
+
+print(my_bool)
+
+# Basic if statement
+if my_bool:
+    print("My Bool is True!")
+    
+my_bool = False
+    
+# If else statement
+if my_bool:
+    print("My Bool is still true?")
+else:
+    print("My Bool is false")
+    
+another_bool = True
+
+# If, if else, else statement
+if my_bool:
+    print("My Bool is still true?")
+elif:
+    print("Another Bool is true")
+else:
+    print("Nothing is true")
+    
+# If statements with logical operators
+if 1 == 2:
+    print("Wait, 1 is equal to 1?")
+elif 1 == 1:
+    print("1 is equal to 1")
+else:
+    print("What does 1 equal?")
+
+# My bool is the value of is 5 less than or equal to 6
+my_bool = 5 <= 6
+
+print(my_bool)
